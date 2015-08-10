d4ba2edea6e829abea49f3e4ccb37874dd2149a8
Chris Mizerak <chris.mizerak@etcconnect.com>
2015-08-10 14:03:34 -0700

    fix for getting incoming UDP source on Mac

--- OSCRouter/MainWindow.cpp
+++ OSCRouter/MainWindow.cpp
@@ -32,7 +32,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#define APP_VERSION				"0.1"
+#define APP_VERSION				"0.2"
 #define SETTING_LOG_DEPTH		"LogDepth"
 #define SETTING_FILE_DEPTH		"FileDepth"
 #define SETTING_LAST_FILE		"LastFile"
