7b9f15d0c67f74e41fae7778fccee7682b111e6f
Miz <chrismizerak@gmail.com>
2018-05-04 16:00:00 -0700

    build

--- OSCRouter/Router.h
+++ OSCRouter/Router.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Electronic Theatre Controls, Inc., http://www.etcconnect.com
+// Copyright (c) 2018 Electronic Theatre Controls, Inc., http://www.etcconnect.com
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
